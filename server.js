f6a0ca69beaeb6564e7b6d67b4a80b95f54aca01
David Llop <llop-no-spam-please@protonmail.com>
2017-03-27 16:21:09 +0200

    Completed API and tests

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var app = express()
 
 app.use(bodyParser.json());
 
+var invalidCredentials = (req, res) => {
+  if (!req.headers.token || !req.headers.token === "c778aacd6c1d6007643d32cbed8afbb8") {
+    res.status(401).json({error: "Invalid credentials"})
+    return true
+  }
+}
+
 app.get('/fake', (req, res) => {
   res.json({version: 1})
 })
@@ -13,35 +20,67 @@ app.post('/fake/register', (req, res) => {
     res.status(422).json({error: "Invalid email"})
     return
   }
-  res.json({uuid: "ae478f75"})
+  if (req.body.user.email === 'used@e.mail') {
+    res.status(442).json({error: "Email already in use"})
+  }
+  res.json({uuid: "ae478f75", token: "c778aacd6c1d6007643d32cbed8afbb8"})
 })
 
 app.post('/fake/register/:uuid', (req, res) => {
-  res.json({})
+  if (invalidCredentials(req, res)) return
+  if (!req.body.user || !req.body.user.signatures
+    || req.body.user.signatures.length != 5 || !Array.isArray(req.body.user.signatures[0])) {
+    res.status(422).json({error: "Invalid signatures"})
+    return
+  }
+  res.json({success: true})
 })
 
 app.post('/fake/login', (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(401).json({error: "Invalid credentials"})
+    return
+  }
   res.json({token: "c778aacd6c1d6007643d32cbed8afbb8", uuid: "ae478f75"})
 })
 
 app.post('/fake/request', (req, res) => {
+  if (invalidCredentials(req, res)) return
+  if (!req.body.document || !req.body.document.id) {
+    console.error("no document id")
+  }
   res.json({})
 })
 
 app.get('/fake/request', (req, res) => {
-  res.json({requests: ["ae478f75"]})
+  if (invalidCredentials(req, res)) return
+  res.json({requests: ["5b83d596a85edc1a35f9592e9cd19212", "a6f92c80f70b7f9da5997e618fb57e0a"]})
 })
 
 app.post('/fake/request/:id', (req, res) => {
+  if (invalidCredentials(req, res)) return
+  if (req.params.id > 10) {
+    res.status(404).json({error: "Request not found"})
+    return
+  }
+  if (!req.body.signature || !Array.isArray(req.body.signature)) {
+    res.status(401).json({error: "Invalid signature"})
+    return
+  }
   res.json({})
 })
 
 app.get('/fake/request/:id', (req, res) => {
+  if (invalidCredentials(req, res)) return
+  if (req.params.id > 10) {
+    res.status(404).json({error: "Request not found"})
+    return
+  }
   try {
     let signature = require('./stubs/sig'+parseInt(req.params.id)+'.json')
     res.json({signature})
   } catch(e) {
-    res.status(404).json({error: "Signature not found"})
+    res.status(204) // No content
   }
 })
 
--- test.js
+++ test.js
@@ -3,10 +3,20 @@ var request = require('supertest')
 
 var app = require('./server')
 
-describe('Routes', () => {
-  describe('/fake', () => {
-    it('should return the version number', (done) => {
+let invalidCredentials = (url, method = 'POST') => {
+  return (done) => {
+    (method !== 'GET' ? request(app).post(url) : request(app).get(url))
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .then((res) => {
+        done()
+      })
+  }
+}
 
+describe('Routes', () => {
+  describe('GET /fake', () => {
+    it('should return the version number', (done) => {
       request(app)
 	      .get('/fake')
         .expect(200)
@@ -16,8 +26,8 @@ describe('Routes', () => {
         })
     })
   })
-  describe('/fake/register', () => {
-    it('should correctly create an existing account', (done) => {
+  describe('POST /fake/register', () => {
+    it('should correctly create an account', (done) => {
 	    var body = {
 		    user: {
           email: "test@easysign.com",
@@ -42,6 +52,175 @@ describe('Routes', () => {
         .send(body)
         .expect('Content-Type', /json/)
         .expect(422)
+        .then((res) => {
+          done()
+        })
+    })
+  })
+  describe('POST /fake/register/:uuid', () => {
+    it('should return an error if invalid credentials', invalidCredentials('/fake/register/ae478f75'))
+    it('should return an error if invalid signature set', (done) => {
+      let body = {
+        user: {
+          signatures: [[1], [2], [3], [4]]
+        }
+      }
+      request(app)
+        .post('/fake/register/ae478f75')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(422)
+        .then((res) => {
+          done()
+        })
+    })
+    it('should return success if correct signature set', (done) => {
+      let body = {
+        user: {
+          signatures: [[1], [2], [3], [4], [5]]
+        }
+      }
+      request(app)
+        .post('/fake/register/ae478f75')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((res) => {
+          done()
+        })
+    })
+  })
+  describe('POST /fake/login', () => {
+    it('should return an error if invalid credentails', invalidCredentials('/fake/login'))
+    it('should return a token and the uuid if correct credentials', (done) => {
+      let body = {
+        email: "test@easysign.com",
+        password: "password"
+      }
+      request(app)
+        .post('/fake/login')
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect((res) => {
+          res.body.token.should.be.equal("c778aacd6c1d6007643d32cbed8afbb8")
+          res.body.uuid.should.be.equal("ae478f75")
+        })
+        .then((res) => {
+          done()
+        })
+    })
+  })
+  describe('POST /fake/request', () => {
+    it('should return an error if invalid credentials', invalidCredentials('/fake/request', 'GET'))
+    it('should accept a document id', (done) => {
+      let body = {
+        document: {
+          id: "5b83d596a85edc1a35f9592e9cd19212"
+        }
+      }
+      request(app)
+        .post('/fake/request')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((res) => {
+          done()
+        })
+    })
+  })
+  describe('GET /fake/request', () => {
+    it('should return an error if invalid credentials', invalidCredentials('/fake/request/1'))
+    it('should return an array of request ids if correct credetials', (done) => {
+      request(app)
+        .get('/fake/request')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect((res) => {
+          res.body.requests.should.be.deepEqual(["5b83d596a85edc1a35f9592e9cd19212", "a6f92c80f70b7f9da5997e618fb57e0a"])
+        })
+        .then((res) => {
+          done()
+        })
+    })
+  })
+  describe('POST /fake/request/:id', () => {
+    it('should return an error if invalid credentials', invalidCredentials('/fake/request/1'))
+    it('should return an error if invalid signature', (done) => {
+      let body = {
+        signature: "invalid"
+      }
+      request(app)
+        .post('/fake/request/1')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .then((res) => {
+          done()
+        })
+    })
+    it('should return an error if document not found', (done) => {
+      let body = {
+        signature: [["valid"]]
+      }
+      request(app)
+        .post('/fake/request/40')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .then((res) => {
+          done()
+        })
+    })
+    it('should accept a correct signature', (done) => {
+      let body = {
+        signature: [["valid"]]
+      }
+      request(app)
+        .post('/fake/request/4')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((res) => {
+          done()
+        })
+    })
+  })
+  describe('GET /fake/request/:id', () => {
+    it('should return an error if invalid credentials', invalidCredentials('/fake/request/1', 'GET'))
+    it('should return an error if request not found', (done) => {
+      request(app)
+        .get('/fake/request/40')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .then((res) => {
+          done()
+        })
+    })
+    it('should return a no content if no signature is present', (done) => {
+      request(app)
+        .get('/fake/request/4')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .expect(204)
+        done()
+    })
+    it('should return the signature if it is present', (done) => {
+      request(app)
+        .get('/fake/request/3')
+        .set('token', 'c778aacd6c1d6007643d32cbed8afbb8')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect((res) => {
+          res.body.should.have.property('signature').which.is.an.Array()
+        })
         .then((res) => {
           done()
         })
